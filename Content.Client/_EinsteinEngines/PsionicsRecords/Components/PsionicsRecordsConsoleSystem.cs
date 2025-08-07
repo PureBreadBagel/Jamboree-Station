de86b2363bd2509c4fccd74c417646aebc05fd74
Baine Junk <wym0n@proton.me>
2025-08-06 22:58:00 -0400

    Records console fixes

---
+++ Content.Client/_EinsteinEngines/PsionicsRecords/Components/PsionicsRecordsConsoleSystem.cs
@@ -0,0 +1,11 @@
+using Content.Shared._EinsteinEngines.PsionicsRecords.Systems;
+
+/// <summary>
+/// EVERYTHING HERE IS A MODIFIED VERSION OF CRIMINAL RECORDS
+/// </summary>
+
+namespace Content.Client._EinsteinEngines.PsionicsRecords.Systems;
+
+public sealed class PsionicsRecordsConsoleSystem : SharedPsionicsRecordsConsoleSystem
+{
+}
