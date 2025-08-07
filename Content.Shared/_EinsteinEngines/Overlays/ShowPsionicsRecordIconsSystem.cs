59e81a49bbea8bd9223893c44592841caf7fcfae
JamboreeBot <JamboreeBot@proton.me>
2025-08-07 04:35:34 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/_EinsteinEngines/Overlays/ShowPsionicsRecordIconsSystem.cs
+++ Content.Shared/_EinsteinEngines/Overlays/ShowPsionicsRecordIconsSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Baine Junk <wym0n@proton.me>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.GameStates;
 
 /// <summary>
--- Content.Shared/_EinsteinEngines/Psionics/Components/PsionicsRecordComponent.cs
+++ Content.Shared/_EinsteinEngines/Psionics/Components/PsionicsRecordComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Baine Junk <wym0n@proton.me>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.StatusIcon;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
