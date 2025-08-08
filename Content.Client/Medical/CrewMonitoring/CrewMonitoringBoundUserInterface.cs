313d8888492b8cbb47c789083b19179d6d90b98d
JamboreeBot <JamboreeBot@proton.me>
2025-08-08 03:10:58 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Medical/CrewMonitoring/CrewMonitoringBoundUserInterface.cs
+++ Content.Client/Medical/CrewMonitoring/CrewMonitoringBoundUserInterface.cs
@@ -11,6 +11,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Baine Junk <wym0n@proton.me>
+// SPDX-FileCopyrightText: 2025 JamboreeBot <JamboreeBot@proton.me>
 // SPDX-FileCopyrightText: 2025 Milon <milonpl.git@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
