7a7707dd8cf1a93f60e03f6a0a7cbf46f6eca264
Baine Junk <wym0n@proton.me>
2025-08-07 23:07:02 -0400

    Port LPO from DV

--- Content.Client/Medical/CrewMonitoring/CrewMonitoringBoundUserInterface.cs
+++ Content.Client/Medical/CrewMonitoring/CrewMonitoringBoundUserInterface.cs
@@ -14,6 +14,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using Content.Shared._DV.Medical.CrewMonitoring; // DeltaV
 using Content.Shared.Medical.CrewMonitoring;
 using Robust.Client.UserInterface;
 
@@ -38,6 +39,14 @@ public sealed class CrewMonitoringBoundUserInterface : BoundUserInterface
         if (EntMan.TryGetComponent<TransformComponent>(Owner, out var xform))
         {
             gridUid = xform.GridUid;
+
+            // Begin DeltaV Additions - Find a station's grid instead of the current one for evil monitors
+            if (EntMan.HasComponent<LongRangeCrewMonitorComponent>(Owner))
+            {
+                gridUid = EntMan.System<LongRangeCrewMonitorSystem>().FindStationGridInMap(xform.MapID);
+                gridUid ??= xform.GridUid; // fall back to whatever grid this is on if it failed
+            }
+            // End DeltaV Additions
 
             if (EntMan.TryGetComponent<MetaDataComponent>(gridUid, out var metaData))
             {
