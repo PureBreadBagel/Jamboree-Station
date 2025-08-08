10e70f5c5a7b72e43de8add59dd257e8f4986f51
JamboreeBot <JamboreeBot@proton.me>
2025-08-08 03:09:42 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_DV/StationEvents/Components/DebrisSpawnerRuleComponent.cs
+++ Content.Server/_DV/StationEvents/Components/DebrisSpawnerRuleComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Baine Junk <wym0n@proton.me>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 /*
  * Delta-V - This file is licensed under AGPLv3
  * Copyright (c) 2024 Delta-V Contributors
