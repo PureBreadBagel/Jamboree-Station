8e0d565375a84db3168dc880ca9e5663eced4e73
JamboreeBot <JamboreeBot@proton.me>
2025-08-16 04:52:03 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Chat/Managers/IChatManager.cs
+++ Content.Server/Chat/Managers/IChatManager.cs
@@ -99,6 +99,7 @@
 // SPDX-FileCopyrightText: 2024 themias <89101928+themias@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 Арт <123451459+JustArt1m@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Baine Junk <wym0n@proton.me>
 // SPDX-FileCopyrightText: 2025 JamboreeBot <JamboreeBot@proton.me>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 Simon <63975668+simyon264@users.noreply.github.com>
