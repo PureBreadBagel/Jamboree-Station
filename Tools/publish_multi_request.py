eec2d69f9da728cbaab8d2369728aff54f1a24ac
Baine Junk <wym0n@proton.me>
2025-08-19 21:51:08 -0400

    update publish workflow

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -20,7 +20,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.yeen.dev/"
+ROBUST_CDN_URL = "https://cdn.jamboree.lol/"
 FORK_ID = "jamboree"
 
 def main():
