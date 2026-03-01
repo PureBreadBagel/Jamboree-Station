fc255247b508d5910807027d47ff366690430585
JohnJohn <189290423+JohnJJohn@users.noreply.github.com>
2026-03-01 21:49:35 +0000

    Phones (#909)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- Content.Shared/Chat/ISharedChatManager.cs
+++ Content.Shared/Chat/ISharedChatManager.cs
@@ -10,4 +10,20 @@ public interface ISharedChatManager
     void Initialize();
     void SendAdminAlert(string message);
     void SendAdminAlert(EntityUid player, string message);
+
+    /// <summary>
+    /// This is a dangerous function! Only pass in property escaped text.
+    /// See: <see cref="SendAdminAlert(string)"/>
+    /// <br/><br/>
+    /// Use this for things that need to be unformatted (like tpto links) but ensure that everything else
+    /// is formated properly. If it's not, players could sneak in ban links or other nasty commands that the admins
+    /// could clink on.
+    /// </summary>
+    void SendAdminAlertNoFormatOrEscape(string message);
+
+    /// <summary>
+    /// Trauma - moved from server, sends a message to a single player.
+    /// </summary>
+    void ChatMessageToOne(ChatChannel channel, string message, string wrappedMessage, EntityUid source, bool hideChat,
+        INetChannel client, Color? colorOverride = null, bool recordReplay = false, string? audioPath = null, float audioVolume = 0, NetUserId? author = null, bool canCoalesce = true, bool hidePopup = false); // Trauma added hidePopup
 }
