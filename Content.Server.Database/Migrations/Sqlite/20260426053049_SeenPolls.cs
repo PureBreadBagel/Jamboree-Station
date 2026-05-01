4fdbc7bc068f28c7ce8dfc5bdc5132e30c379570
JamboreeBot <JamboreeBot@proton.me>
2026-05-01 02:49:37 +0000

    chore: Automatically update REUSE headers

--- Content.Server.Database/Migrations/Sqlite/20260426053049_SeenPolls.cs
+++ Content.Server.Database/Migrations/Sqlite/20260426053049_SeenPolls.cs
@@ -1,4 +1,8 @@
-﻿using System;
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
