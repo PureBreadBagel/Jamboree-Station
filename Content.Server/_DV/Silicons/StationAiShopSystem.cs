dbaecaa3e8ccab8b02502e9e4b4530a7cdc34a7e
JamboreeBot <JamboreeBot@proton.me>
2026-05-25 21:23:15 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_DV/Silicons/StationAiShopSystem.cs
+++ Content.Server/_DV/Silicons/StationAiShopSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Fluids.EntitySystems;
 using Content.Server.Light.Components;
 using Content.Server.Light.EntitySystems;
