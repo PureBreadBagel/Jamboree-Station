c891946c7cfe37d0ec7dbebab62c39628bfc32d6
PureBreadBagel <146181383+PureBreadBagel@users.noreply.github.com>
2026-06-01 14:06:34 +0100

    Ehhh

--- Content.Shared/Containers/ContainerFillSystem.cs
+++ Content.Shared/Containers/ContainerFillSystem.cs
@@ -80,7 +80,7 @@ using System.Numerics;
 using Content.Shared.EntityTable;
 using Robust.Shared.Containers;
 using Robust.Shared.Map;
-using System.Diagnostics;
+
 namespace Content.Shared.Containers;
 
 public sealed class ContainerFillSystem : EntitySystem
@@ -104,18 +104,17 @@ public sealed class ContainerFillSystem : EntitySystem
         var xform = Transform(uid);
         var coords = new EntityCoordinates(uid, Vector2.Zero);
 
-        foreach (var (containerId, prototypes) in component.Containers)
+        foreach (var (contaienrId, prototypes) in component.Containers)
         {
-            if (!_containerSystem.TryGetContainer(uid, containerId, out var container, containerComp))
+            if (!_containerSystem.TryGetContainer(uid, contaienrId, out var container, containerComp))
             {
-                Log.Error($"Entity {ToPrettyString(uid)} with a {nameof(ContainerFillComponent)} is missing a container ({containerId}).");
+                Log.Error($"Entity {ToPrettyString(uid)} with a {nameof(ContainerFillComponent)} is missing a container ({contaienrId}).");
                 continue;
             }
 
             foreach (var proto in prototypes)
             {
                 var ent = Spawn(proto, coords);
-                Debug.Assert(_containerSystem.CanInsert(ent, container));
                 if (!_containerSystem.Insert(ent, container, containerXform: xform))
                 {
                     var alreadyContained = container.ContainedEntities.Count > 0 ? string.Join("\n", container.ContainedEntities.Select(e => $"\t - {ToPrettyString(e)}")) : "< empty >";
@@ -142,7 +141,7 @@ public sealed class ContainerFillSystem : EntitySystem
         {
             if (!_containerSystem.TryGetContainer(ent, containerId, out var container, containerComp))
             {
-
+                Log.Error($"Entity {ToPrettyString(ent)} with a {nameof(EntityTableContainerFillComponent)} is missing a container ({containerId}).");
                 continue;
             }
 
@@ -150,16 +149,12 @@ public sealed class ContainerFillSystem : EntitySystem
             foreach (var proto in spawns)
             {
                 var spawn = Spawn(proto, coords);
-
-
-
-
                 if (!_containerSystem.Insert(spawn, container, containerXform: xform))
                 {
                     var alreadyContained = container.ContainedEntities.Count > 0 ? string.Join("\n", container.ContainedEntities.Select(e => $"\t - {ToPrettyString(e)}")) : "< empty >";
                     Log.Error($"Entity {ToPrettyString(ent)} with a {nameof(EntityTableContainerFillComponent)} failed to insert an entity: {ToPrettyString(spawn)}.\nCurrent contents:\n{alreadyContained}");
                     _transform.AttachToGridOrMap(spawn);
-                    continue;
+                    break;
                 }
             }
         }
