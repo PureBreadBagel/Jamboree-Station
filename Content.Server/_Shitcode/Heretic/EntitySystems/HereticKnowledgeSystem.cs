8839ccdc0d9118ff1e93a2414ce9f0688823f9a8
PureBreadBagel <146181383+PureBreadBagel@users.noreply.github.com>
2026-06-01 23:51:48 +0100

    Revert "Idk why it copied twice."
    This reverts commit 1af86d1c71c336e57dcff184a091fd9286e9c3cb.

--- Content.Server/_Shitcode/Heretic/EntitySystems/HereticKnowledgeSystem.cs
+++ Content.Server/_Shitcode/Heretic/EntitySystems/HereticKnowledgeSystem.cs
@@ -29,7 +29,7 @@ public sealed partial class HereticKnowledgeSystem : EntitySystem
     public HereticKnowledgePrototype GetKnowledge(ProtoId<HereticKnowledgePrototype> id)
         => _proto.Index(id);
 
-    public void AddKnowledge(EntityUid uid, HereticComponent comp, ProtoId<HereticKnowledgePrototype> id, bool silent = true, bool research = true)
+    public void AddKnowledge(EntityUid uid, HereticComponent comp, ProtoId<HereticKnowledgePrototype> id, bool silent = true)
     {
         if (comp.ResearchedKnowledge.Contains(id))
             return;
@@ -38,6 +38,8 @@ public sealed partial class HereticKnowledgeSystem : EntitySystem
 
         if (data.Event != null)
             RaiseLocalEvent(uid, data.Event, true);
+
+        var hasMind = _mind.TryGetMind(uid, out var mind, out _);
 
         if (data.ActionPrototypes != null && data.ActionPrototypes.Count > 0)
         {
@@ -68,35 +70,6 @@ public sealed partial class HereticKnowledgeSystem : EntitySystem
             comp.PathStage = data.Stage;
 
         if (!silent) _popup.PopupEntity(Loc.GetString("heretic-knowledge-gain"), uid, uid);
-        if (research) comp.ResearchedKnowledge.Add(id);
-    }
-    public void RemoveKnowledge(EntityUid uid, HereticComponent comp, ProtoId<HereticKnowledgePrototype> id, bool silent = false)
-    {
-        var data = GetKnowledge(id);
-
-        if (data.ActionPrototypes != null && data.ActionPrototypes.Count > 0)
-        {
-            foreach (var act in data.ActionPrototypes)
-            {
-                comp.ProvidedActions.RemoveAll(action =>
-                {
-                    // goida
-                    if (Prototype(action)?.ID is not {} id || id != act)
-                        return false;
-
-                    _action.RemoveAction(action);
-                    return true;
-                });
-            }
-        }
-
-        if (data.RitualPrototypes != null && data.RitualPrototypes.Count > 0)
-            foreach (var ritual in data.RitualPrototypes)
-                comp.KnownRituals.Remove(_ritual.GetRitual(ritual));
-
-        Dirty(uid, comp);
-
-        if (!silent)
-            _popup.PopupEntity(Loc.GetString("heretic-knowledge-loss"), uid, uid);
+        comp.ResearchedKnowledge.Add(id);
     }
 }
--- Content.Server/_Shitcode/Heretic/EntitySystems/HereticSystem.cs
+++ Content.Server/_Shitcode/Heretic/EntitySystems/HereticSystem.cs
@@ -102,6 +102,14 @@ public sealed class HereticSystem : EntitySystem
         Subs.CVar(_cfg, GoobCVars.AscensionRequiresObjectives, value => _ascensionRequiresObjectives = value, true);
     }
 
+    private void OnPolymorphed(Entity<HereticComponent> ent, ref PolymorphedEvent args)
+    {
+        if (args.IsRevert)
+            return;
+
+        _polymorph.CopyPolymorphComponent<HereticComponent>(ent, args.NewEntity);
+    }
+
     private void OnRestart(RoundRestartCleanupEvent ev)
     {
         _timer = 0f;
@@ -209,11 +217,7 @@ public sealed class HereticSystem : EntitySystem
             _eye.SetVisibilityMask(ent, eye.VisibilityMask | HereticVisFlags, eye);
 
         foreach (var k in ent.Comp.BaseKnowledge)
-            _knowledge.AddKnowledge(ent, ent.Comp, k, research: false);
-
-        // in case of polymorph
-        foreach (var k in ent.Comp.ResearchedKnowledge)
-            _knowledge.AddKnowledge(ent, ent.Comp, k, research: false);
+            _knowledge.AddKnowledge(ent, ent.Comp, k);
 
         if (ent.Comp.KnowledgeRequiredTags.Count == 0)
             GenerateRequiredKnowledgeTags(ent);
