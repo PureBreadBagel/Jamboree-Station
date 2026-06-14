1dd7f2bad9839c2016b2df4d7c2260f87251a824
PureBreadBagel <146181383+PureBreadBagel@users.noreply.github.com>
2026-06-14 23:28:31 +0100

    Oh boy!

--- Content.Server/Atmos/Reactions/InfernazimHeatReaction.cs
+++ Content.Server/Atmos/Reactions/InfernazimHeatReaction.cs
@@ -2,11 +2,11 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+using System;
 using Content.Server.Atmos.EntitySystems;
 using Content.Shared.Atmos;
 using Content.Shared.Atmos.Reactions;
 using JetBrains.Annotations;
-using Robust.Shared.Utility;
 
 namespace Content.Server.Atmos.Reactions;
 
@@ -14,30 +14,71 @@ namespace Content.Server.Atmos.Reactions;
 [DataDefinition]
 public sealed partial class InfernazimHeatReaction : IGasReactionEffect
 {
+    private const float TargetTemperature = 6000f; // The target temp that Infernazim wants to be at. In Kelvin.
+    private const float EnergyPerSecond = 80000f; // How much energy Infernazim generates so it can get to the target temp.
+
+    private const float PassiveDecayPerSecond = 0.02f; // The mole decay rate of Infernazim when nothing is touching it.
+    private const float ConversionPerSecond = 0.25f; // The mole conversion rate of Infernazim when it is touching Oxygen or Kaltrenoxide.
+
     public ReactionResult React(
-        GasMixture mixture,
-        IGasMixtureHolder? holder,
-        AtmosphereSystem atmosphereSystem,
-        float reactionDelta)
+        GasMixture mixture, // Mixture is basically how much gas is in the tile.
+        IGasMixtureHolder? holder, // Holder is the entity that holds the gas mixture, such as a tile or a container.
+        AtmosphereSystem atmosphereSystem, // AtmosphereSystem is the system that handles all the gas reactions and other gas related stuff.
+        float reactionDelta) // reactionDelta is the time since the last reaction, in seconds.
     {
         if (reactionDelta <= 0f)
             return ReactionResult.NoReaction;
 
-        var temperature = mixture.Temperature;
+        var reacted = false;
+
+        var infernazim = mixture.GetMoles(Gas.Infernazim); // Get the amount of Infernazim in the mixture.
+        if (infernazim <= 0f)
+            return ReactionResult.NoReaction; // If there is no Infernazim in the mixture, then we don't want to do anything.
+
+        var oxygen = mixture.GetMoles(Gas.Oxygen); // Get the amount of Oxygen in the mixture.
+        var kaltrenoxide = mixture.GetMoles(Gas.Kaltrenoxide); // Get the amount of Kaltrenoxide in the mixture.
 
 
-        const float targetTemperature = 6000f;
 
-        if (temperature >= targetTemperature)
-            return ReactionResult.NoReaction;
+        var catalyst = oxygen + kaltrenoxide; // The catalyst is the sum of Oxygen and Kaltrenoxide, which are the gases that Infernazim reacts with.
 
-        var heatCapacity = atmosphereSystem.GetHeatCapacity(mixture, true);
-        if (heatCapacity <= Atmospherics.MinimumHeatCapacity)
-         return ReactionResult.NoReaction;
+        if (catalyst > 0f)
+        {
+            var catalystFactor = MathF.Min(1f, catalyst / 5f);
 
-        const float energyPerSecond = 80000f;
+            var converted = MathF.Min(
+                infernazim,
+                ConversionPerSecond * reactionDelta * catalystFactor); // The amount of Infernazim that will be converted to Water Vapor, based on the amount of catalyst present and the reaction delta.
 
-        mixture.Temperature += (energyPerSecond * reactionDelta) / heatCapacity;
-        return ReactionResult.Reacting;
+            if (converted > 0f)
+            {
+                mixture.AdjustMoles(Gas.Infernazim, -converted); // Remove the converted amount of Infernazim from the mixture.
+                mixture.AdjustMoles(Gas.WaterVapor, converted); // Ditto but oposite. Add water vapor instead lol.
+                reacted = true;
+
+                infernazim -= converted; // DELETE THE INFERNAZIUM RAHHHH
+            }
+        }
+
+
+        var decay = MathF.Min(infernazim, PassiveDecayPerSecond * reactionDelta); // The amount of Infernazim that will decay away, based on the passive decay rate and the reaction delta.
+        if (decay > 0f)
+        {
+            mixture.AdjustMoles(Gas.Infernazim, -decay);
+            reacted = true; // Infernazim decays away over time, even when nothing is touching it...Too bad not even the dying star can stop its own decay huh?
+        }
+
+        if (infernazim > 0f && mixture.Temperature < TargetTemperature)
+        {
+            var heatCapacity = atmosphereSystem.GetHeatCapacity(mixture, true); // Get the heat capacity of the mixture, which is how much energy it takes to change the temperature of the mixture by 1 degree.
+
+            if (heatCapacity > Atmospherics.MinimumHeatCapacity)
+            {
+                mixture.Temperature += EnergyPerSecond * reactionDelta / heatCapacity; // GRRRR HEAT IT UP RAHH!
+                reacted = true;
+            }
+        }
+
+        return reacted ? ReactionResult.Reacting : ReactionResult.NoReaction; // Return whether or not the reaction actually did anything.
     }
 }
--- Content.Server/Atmos/Reactions/KaltrenoxideCoolantReaction.cs
+++ Content.Server/Atmos/Reactions/KaltrenoxideCoolantReaction.cs
@@ -6,7 +6,6 @@ using Content.Server.Atmos.EntitySystems;
 using Content.Shared.Atmos;
 using Content.Shared.Atmos.Reactions;
 using JetBrains.Annotations;
-using System;
 
 namespace Content.Server.Atmos.Reactions;
 
@@ -14,31 +13,56 @@ namespace Content.Server.Atmos.Reactions;
 [DataDefinition]
 public sealed partial class KaltrenoxideCoolantReaction : IGasReactionEffect
 {
+    private const float TargetTemperature = 73.15f; // The target temp that Kaltrenoxide wants to be at.
+    private const float CoolingPerSecond = 80000f; // How much energy Kalt removes so it can get to the target temp.
+
+    // Slow passive decay, even when nothing is touching it.
+    private const float PassiveDecayPerSecond = 0.02f;
+
+    // Extra decay when Nitrogen is present.
+    private const float NitrogenDecayPerSecond = 0.25f;
+
     public ReactionResult React(
-        GasMixture mixture,
-        IGasMixtureHolder? holder,
-        AtmosphereSystem atmosphereSystem,
-        float reactionDelta)
+        GasMixture mixture, // Mixture is basically how much gas is in the tile.
+        IGasMixtureHolder? holder, // Holder is the entity that holds the gas mixture, such as a tile or a container.
+        AtmosphereSystem atmosphereSystem, // AtmosphereSystem is the system that handles all the gas reactions and other gas related stuff.
+        float reactionDelta) // reactionDelta is the time since the last reaction, in seconds.
     {
         if (reactionDelta <= 0f)
-            return ReactionResult.NoReaction;
+            return ReactionResult.NoReaction; // If the reactionDelta is less than or equal to 0, then we don't want to do anything.
 
-        var temperature = mixture.Temperature;
+        var reacted = false;
 
-        // Cold but still safely above absolute zero
-        const float targetTemperature = 73.15f;
+        // Normal cooling behavior.
+        if (mixture.Temperature > TargetTemperature)
+        {
+            var heatCapacity = atmosphereSystem.GetHeatCapacity(mixture, true); // Get the heat capacity of the mixture, which is how much energy it takes to change the temperature of the mixture by 1 degree.
+            if (heatCapacity > Atmospherics.MinimumHeatCapacity) // If the heat capacity is too low, we don't want to cool it down because it will just make it worse.
+            {
+                mixture.Temperature -= CoolingPerSecond * reactionDelta / heatCapacity; // Cool the mixture down towards the target temperature.
+                reacted = true;
+            }
+        }
 
-        if (temperature <= targetTemperature)
-            return ReactionResult.NoReaction;
+        // Passive decay over time.
+        var passiveDecay = PassiveDecayPerSecond * reactionDelta;
+        if (passiveDecay > 0f)
+        {
+            mixture.AdjustMoles(Gas.Kaltrenoxide, -passiveDecay); // Kaltrenoxide decays away...
+            reacted = true;
+        }
 
-        var heatCapacity = atmosphereSystem.GetHeatCapacity(mixture, true);
+        // Nitrogen kills Kaltrenoixde faster! >:]
+        var nitrogenMoles = mixture.GetMoles(Gas.Nitrogen); // Get the amount of Nitrogen in the mixture.
+        if (nitrogenMoles > 0f)
+        {
+            var nitrogenFactor = MathF.Min(1f, nitrogenMoles / 5f);
+            var nitrogenDecay = NitrogenDecayPerSecond * reactionDelta * nitrogenFactor;
 
-        if (heatCapacity <= Atmospherics.MinimumHeatCapacity)
-        return ReactionResult.NoReaction;
+            mixture.AdjustMoles(Gas.Kaltrenoxide, -nitrogenDecay); // Kaltrenoxide dies due to nitrogens cringyness.
+            reacted = true;
+        }
 
-        const float coolingPerSecond = 80000f;
-        mixture.Temperature -= (coolingPerSecond * reactionDelta) / heatCapacity;
-
-        return ReactionResult.Reacting;
+        return reacted ? ReactionResult.Reacting : ReactionResult.NoReaction; // Return whether or not the reaction actually did anything.
     }
 }
