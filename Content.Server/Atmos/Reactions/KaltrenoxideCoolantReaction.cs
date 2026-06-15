e841070d00bc311e0794496fcebcc697eee2bc03
PureBreadBagel <146181383+PureBreadBagel@users.noreply.github.com>
2026-06-15 19:41:12 +0100

    NO WAY...

--- Content.Server/Atmos/Reactions/KaltrenoxideCoolantReaction.cs
+++ Content.Server/Atmos/Reactions/KaltrenoxideCoolantReaction.cs
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
-//
+// SPDX-FileCopyrightText: 2026 PureBreadBagel <PureBreadBagel@no=reply.github.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
+//
 
 using Content.Server.Atmos.EntitySystems;
 using Content.Shared.Atmos;
