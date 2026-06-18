c04d1c485ca4c9711a1e37bff1d91ee439f7cf46
JamboreeBot <JamboreeBot@proton.me>
2026-06-18 14:29:49 +0000

    chore: Automatically update REUSE headers

--- Content.Client/_DV/Paper/UI/PaperBoundUserInterface.cs
+++ Content.Client/_DV/Paper/UI/PaperBoundUserInterface.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Prototypes;
 using Content.Shared.Chat.TypingIndicator;
 using Content.Client.Chat.TypingIndicator;
