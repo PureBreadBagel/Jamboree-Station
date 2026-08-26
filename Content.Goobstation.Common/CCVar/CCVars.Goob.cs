e89339150a3b0b8c13f4eaa27c66716f6cf4bfb2
PureBreadBagel <146181383+PureBreadBagel@users.noreply.github.com>
2026-08-26 18:43:09 +0100

    CCvard

--- Content.Goobstation.Common/CCVar/CCVars.Goob.cs
+++ Content.Goobstation.Common/CCVar/CCVars.Goob.cs
@@ -675,7 +675,7 @@ public sealed partial class GoobCVars
     /// The distance to spawn a space whale from the station
     /// </summary>
     public static readonly CVarDef<int> SpaceWhaleSpawnDistance =
-        CVarDef.Create("misc.space_whale_spawn_distance", 1965, CVar.SERVER);
+        CVarDef.Create("misc.space_whale_spawn_distance", 3000, CVar.SERVER);
 
     #endregion
     /// <summary>
