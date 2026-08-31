50465430bda0d1b847e67840875e6a9d39efdcb9
JamboreeBot <JamboreeBot@proton.me>
2026-08-31 00:48:54 +0000

    chore: Automatically update REUSE headers

--- Content.Server/CartridgeLoader/Cartridges/NanoTaskCartridgeSystem.cs
+++ Content.Server/CartridgeLoader/Cartridges/NanoTaskCartridgeSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.CartridgeLoader.Cartridges;
 using Content.Shared.CartridgeLoader;
 using Content.Shared.Hands.EntitySystems;
